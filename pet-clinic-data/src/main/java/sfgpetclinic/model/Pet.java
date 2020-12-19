efc6350875673ec8b85e0e3d1551d658fd9265ab
hasanyesil <hasannyesil97@gmail.com>
2020-12-20 02:11:08 +0300

    adding base entity. closes #9.

--- pet-clinic-data/src/main/java/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/sfgpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package sfgpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
 
     private PetType petType;
     private Owner owner;
--- pet-clinic-data/src/main/java/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
