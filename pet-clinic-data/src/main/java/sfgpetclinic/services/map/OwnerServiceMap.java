5025b647d9b03f8ef27c64da835414e14578e96b
hasanyesil <hasannyesil97@gmail.com>
2021-01-02 02:25:02 +0300

    Impl Map Based Services, closes #5.

---
+++ pet-clinic-data/src/main/java/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,33 @@
+package sfgpetclinic.services.map;
+
+import sfgpetclinic.model.Owner;
+import sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
