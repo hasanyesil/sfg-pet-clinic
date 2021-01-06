061f3bc7afe74b2dbc76d603b59f3c42a76e45fb
hasanyesil <hasannyesil97@gmail.com>
2021-01-06 16:17:41 +0300

    adding bootstrap for data. Closes #13.

--- pet-clinic-data/src/main/java/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -1,11 +1,11 @@
 package sfgpetclinic.services.map;
 
 import sfgpetclinic.model.Owner;
-import sfgpetclinic.services.CrudService;
+import sfgpetclinic.services.OwnerService;
 
 import java.util.Set;
 
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
@@ -29,5 +29,10 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
     @Override
     public Owner findById(Long id) {
         return super.findById(id);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return null;
     }
 }
--- pet-clinic-data/src/main/java/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/sfgpetclinic/services/map/VetServiceMap.java
@@ -1,11 +1,11 @@
 package sfgpetclinic.services.map;
 
 import sfgpetclinic.model.Vet;
-import sfgpetclinic.services.CrudService;
+import sfgpetclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet,Long> implements CrudService<Vet,Long> {
+public class VetServiceMap extends AbstractMapService<Vet,Long> implements VetService{
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
