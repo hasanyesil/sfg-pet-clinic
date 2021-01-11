a583769e8e7f6f3f5a2189999e46ad87f9f5c48a
hasanyesil <hasannyesil97@gmail.com>
2021-01-11 11:10:31 +0300

    updated vet with Jpa entity mappings . Closes #36.

--- pet-clinic-data/src/main/java/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/sfgpetclinic/model/Speciality.java
@@ -1,7 +1,14 @@
 package sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity{
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/sfgpetclinic/model/Vet.java
@@ -1,10 +1,15 @@
 package sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
 
+    @ManyToMany(fetch = FetchType.EAGER)    //Many to many must be eager.
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
